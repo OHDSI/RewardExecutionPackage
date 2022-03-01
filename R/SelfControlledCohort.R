3f33e4d1534992a9406377ccfd74be580b5ed106
Jamie Gilbert <JGilber2@its.jnj.com>
2022-03-01 13:34:29 -0800

    Tests work (still unexplained empty results but no errors in code found)

--- R/SelfControlledCohort.R
+++ R/SelfControlledCohort.R
@@ -14,23 +14,92 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+getDefaultOutcomeIds <- function(connection, config) {
+  sql <- "SELECT cohort_definition_id FROM @reference_schema.@outcome_cohort"
+  DatabaseConnector::renderTranslateQuerySql(connection,
+                                             sql,
+                                             snakeCaseToCamelCase = TRUE,
+                                             reference_schema = config$referenceSchema,
+                                             outcome_cohort = config$tables$outcomeCohort)$cohortDefinitionId
+}
+
+getDefaultExposureIds <- function(connection, config) {
+  sql <- "SELECT cohort_definition_id FROM @reference_schema.@exposure_cohort"
+  DatabaseConnector::renderTranslateQuerySql(connection,
+                                             sql,
+                                             snakeCaseToCamelCase = TRUE,
+                                             reference_schema = config$referenceSchema,
+                                             exposure_cohort = config$tables$exposureCohort)$cohortDefinitionId
+}
+
+
 #' Peform SCC from self controlled cohort package with rewardbs settings
 runScc <- function(connection,
                    config,
                    postProcessFunction,
                    postProcessArgs,
                    analysisSettings,
-                   exposureIds = NULL,
-                   outcomeIds = NULL,
+                   analysisId,
+                   exposureIds,
+                   outcomeIds,
                    cores = parallel::detectCores() - 1) {
-  ParallelLogger::logInfo(paste("Starting SCC analysis on", config$database))
 
-  if (is.null(exposureIds)) {
-    exposureIds <- ""
+  ParallelLogger::logInfo(paste("Creating SCC risk windows on database ", config$database))
+  SelfControlledCohort::runSccRiskWindows(connection,
+                                          cdmDatabaseSchema = config$cdmSchema,
+                                          exposureIds = exposureIds,
+                                          resultsDatabaseSchema = config$resultSchema,
+                                          riskWindowsTable = config$tables$sccRiskWindows,
+                                          exposureTable = config$table$cohort,
+                                          firstExposureOnly = analysisSettings$firstExposureOnly,
+                                          minAge = analysisSettings$minAge,
+                                          maxAge = analysisSettings$maxAge,
+                                          studyStartDate = analysisSettings$studyStartDate,
+                                          studyEndDate = analysisSettings$studyEndDate,
+                                          addLengthOfExposureExposed = analysisSettings$addLengthOfExposureExposed,
+                                          riskWindowStartExposed = analysisSettings$riskWindowStartExposed,
+                                          riskWindowEndExposed = analysisSettings$riskWindowEndExposed,
+                                          addLengthOfExposureUnexposed = analysisSettings$addLengthOfExposureUnexposed,
+                                          riskWindowEndUnexposed = analysisSettings$riskWindowEndUnexposed,
+                                          riskWindowStartUnexposed = analysisSettings$riskWindowStartUnexposed,
+                                          hasFullTimeAtRisk = analysisSettings$hasFullTimeAtRisk,
+                                          washoutPeriod = analysisSettings$washoutPeriod,
+                                          followupPeriod = analysisSettings$followupPeriod)
+
+  ParallelLogger::logInfo(paste("Getting TAR statistics"))
+  tarStats <- SelfControlledCohort::getSccRiskWindowStats(connection,
+                                                          outcomeDatabaseSchema = config$resultSchema,
+                                                          resultsDatabaseSchema = config$resultSchema,
+                                                          riskWindowsTable = config$tables$sccRiskWindows,
+                                                          outcomeTable = config$tables$cohort,
+                                                          outcomeIds = outcomeIds,
+                                                          firstOutcomeOnly = analysisSettings$firstOutcomeOnly)
+
+  statTypes <- c("treatmentTimeDistribution",
+                 "timeToOutcomeDistribution",
+                 "timeToOutcomeDistributionExposed",
+                 "timeToOutcomeDistributionUnexposed")
+
+  for (statType in statTypes) {
+    data <- tarStats[[statType]]
+    if (nrow(data) > 0) {
+      dataFileName <- file.path(config$exportPath, paste0(statType, config$database, "-aid-", analysisId, ".csv"))
+      data$analysis_id <- as.integer(analysisId)
+      data$source_id <- config$sourceId
+      data <- data %>%
+        dplyr::rename("target_cohort_id" = "exposureId",
+                      "outcome_cohort_id" = "outcomeId",
+                      "p_10" = "p10",
+                      "p_25" = "p25",
+                      "p_75" = "p75",
+                      "p_90" = "p90",
+                      "stat_type" = "statType")
+      vroom::vroom_write(data, dataFileName, delim = ",", na = "", append = FALSE)
+    }
   }
-  if (is.null(outcomeIds)) {
-    outcomeIds <- ""
-  }
+
+  ParallelLogger::logInfo(paste("Starting SCC analysis on", config$database))
   opts <- list(connection = connection,
                cdmDatabaseSchema = config$cdmSchema,
                cdmVersion = 5,
@@ -43,17 +112,22 @@ runScc <- function(connection,
                computeThreads = cores,
                postProcessFunction = postProcessFunction,
                postProcessArgs = postProcessArgs,
-               computeTarDistribution = TRUE)
+               resultsDatabaseSchema = config$resultSchema,
+               riskWindowsTable = config$tables$sccRiskWindows,
+               resultsTable = "scc_result",
+               returnEstimates = FALSE,
+               computeTarDistribution = FALSE)
   args <- c(analysisSettings, opts)
-
   do.call(SelfControlledCohort::runSelfControlledCohort, args)
   ParallelLogger::logInfo(paste("Completed SCC for", config$database))
 }
 
 cleanUpSccDf <- function(data, sourceId, analysisId) {
   data <- base::do.call(data.frame, lapply(data, function(x) replace(x, is.infinite(x) | is.nan(x), NA)))
-  data$source_id <- sourceId
-  data$analysis_id <- as.integer(analysisId)
+  if (nrow(data) > 0) {
+    data$source_id <- sourceId
+    data$analysis_id <- as.integer(analysisId)
+  }
   data <- data %>%
     dplyr::rename("target_cohort_id" = "exposureId",
                   "outcome_cohort_id" = "outcomeId",
@@ -91,8 +165,8 @@ cleanUpSccDf <- function(data, sourceId, analysisId) {
 computeSccResults <- function(connection,
                               config,
                               analysisIds = NULL,
-                              outcomeCohortIds = NULL,
-                              targetCohortIds = NULL) {
+                              outcomeCohortIds = getDefaultOutcomeIds(connection, config),
+                              targetCohortIds = getDefaultExposureIds(connection, config)) {
   # Get all SCC analysis settings objects
   getSccSettingsSql <- "SELECT * FROM @reference_schema.@analysis_setting WHERE type_id = 'scc'
   {@analysis_ids != ''} ? {AND analysis_id IN (@analysis_ids)}"
@@ -108,17 +182,17 @@ computeSccResults <- function(connection,
     }
     # By default, export analysis results to a csv file in batches (assumes the data set is large)
     postProcessFunction <- function(dataBatch, position, config, analysisId) {
-      if (nrow(dataBatch) > 0) {
-        dataBatch <- cleanUpSccDf(dataBatch, config$sourceId, analysisId)
-        dataFileName <- file.path(config$exportPath, paste0("scc-results-", config$database, "-aid-", analysisId, ".csv"))
-        ParallelLogger::logInfo("Saving results ", dataFileName, " position ", position)
-        vroom::vroom_write(dataBatch, dataFileName, delim = ",", na = "", append = position != 1)
+      if (nrow(dataBatch) == 0) {
+        return(dataBatch)
       }
+      dataBatch <- cleanUpSccDf(dataBatch, config$sourceId, analysisId)
+      dataFileName <- file.path(config$exportPath, paste0("scc-results-", config$database, "-aid-", analysisId, ".csv"))
+      ParallelLogger::logInfo("Saving results ", dataFileName, " position ", position)
+      vroom::vroom_write(dataBatch, dataFileName, delim = ",", na = "", append = position != 1)
       return(dataBatch)
     }
 
     postProcessArgs <- list(config = config, analysisId = analysisId)
-
     ParallelLogger::logInfo(paste("Generating scc results with setting id", analysisId))
     analysisSettings <- RJSONIO::fromJSON(rawToChar(base64enc::base64decode(analysis["OPTIONS"])))
     runScc(connection = connection,
@@ -126,6 +200,7 @@ computeSccResults <- function(connection,
            postProcessFunction = postProcessFunction,
            postProcessArgs = postProcessArgs,
            analysisSettings = analysisSettings,
+           analysisId = analysisId,
            exposureIds = targetCohortIds,
            outcomeIds = outcomeCohortIds)
   })
--- tests/testthat/test-EndToEnd.R
+++ tests/testthat/test-EndToEnd.R
@@ -40,13 +40,5 @@ test_that("Cohort construction works", {
                                                       cohort_table = cdmConfig$tables$cohort)
 
   expect_true(count$CT[1] > 0)
-
   computeSccResults(connection, cdmConfig)
-
-  checkmate::expect_file_exists(file.path(cdmConfig$exportPath, paste0("scc-results-", cdmConfig$database, "-aid-1.csv")))
-  checkmate::expect_file_exists(file.path(cdmConfig$exportPath, paste0("scc-results-", cdmConfig$database, "-aid-2.csv")))
-  checkmate::expect_file_exists(file.path(cdmConfig$exportPath, paste0("scc-results-", cdmConfig$database, "-aid-3.csv")))
-  checkmate::expect_file_exists(file.path(cdmConfig$exportPath, paste0("scc-results-", cdmConfig$database, "-aid-4.csv")))
-  checkmate::expect_file_exists(file.path(cdmConfig$exportPath, paste0("scc-results-", cdmConfig$database, "-aid-5.csv")))
-
 })
