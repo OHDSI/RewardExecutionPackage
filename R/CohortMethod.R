1a824ab81626e02196c967a1bda5304c2f394e9b
Jamie Gilbert <JGilber2@its.jnj.com>
2024-01-04 14:07:13 -0800

    Removing junk and updating remote branch

--- R/CohortMethod.R
+++ R/CohortMethod.R
@@ -1,3 +1,385 @@
+#' get outcome cohort ids
+#' @description
+#' Return all outcome cohort ids
+#' @param connection DatabaseConnector connection
+#' @param config     cdm configuration
+getAtlasOutcomeIds <- function(connection, config) {
+  sql <- "SELECT oc.cohort_definition_id FROM @reference_schema.@outcome_cohort oc
+  INNER JOIN @reference_schema.@atlas_cohort_reference ar on oc.cohort_definition_id = ar.cohort_definition_id"
+  DatabaseConnector::renderTranslateQuerySql(connection,
+                                             sql,
+                                             snakeCaseToCamelCase = TRUE,
+                                             reference_schema = config$referenceSchema,
+                                             atlas_cohort_reference = config$tables$atlasCohortReference,
+                                             outcome_cohort = config$tables$outcomeCohort)$cohortDefinitionId
+}
+
+createCmDesign <- function(targetId,
+                           comparatorId,
+                           indicationId,
+                           outcomeCohortIds,
+                           excludedCovariateConceptIds) {
+
+  tcis <- list(
+    #standard analyses that would be performed during routine signal detection
+    list(
+      targetId = targetId, # e.g New users of ACE inhibitors
+      comparatorId = comparatorId, # e.g New users of Alpha-1 Blockers
+      indicationId = indicationId, # e.g Hypertension
+      genderConceptIds = c(8507, 8532), # use valid genders (remove unknown)
+      minAge = NULL, # All ages In years. Can be NULL
+      maxAge = NULL, # All ages In years. Can be NULL
+      excludedCovariateConceptIds = excludedCovariateConceptIds
+    )
+  )
+  outcomes <- tibble::tibble(
+    cohortId = outcomeCohortIds,
+    cleanWindow = c(365)
+  )
+
+  timeAtRisks <- tibble::tibble(
+    label = c("On treatment", "fixed 365d"),
+    riskWindowStart = c(1, 1),
+    startAnchor = c("cohort start", "cohort start"),
+    riskWindowEnd = c(0, 365),
+    endAnchor = c("cohort end", "cohort start"),
+  )
+
+  studyStartDate <- "20010101" # YYYYMMDD, e.g. "2001-02-01" for January 1st, 2001
+  studyEndDate <- "20251231" # YYYYMMDD
+
+  useCleanWindowForPriorOutcomeLookback <- FALSE # If FALSE, lookback window is all time prior, i.e., including only first events
+  psMatchMaxRatio <- 1 # If bigger than 1, the outcome model will be conditioned on the matched set
+
+  covariateSettings <- FeatureExtraction::createDefaultCovariateSettings(
+    addDescendantsToExclude = TRUE # Keep TRUE because you're excluding concepts
+  )
+  outcomeList <-
+    lapply(seq_len(nrow(outcomes)), function(i) {
+      if (useCleanWindowForPriorOutcomeLookback)
+        priorOutcomeLookback <- outcomes$cleanWindow[i]
+      else
+        priorOutcomeLookback <- 99999
+      createOutcome(
+        outcomeId = outcomes$cohortId[i],
+        outcomeOfInterest = TRUE,
+        trueEffectSize = NA,
+        priorOutcomeLookback = priorOutcomeLookback
+      )
+    })
+
+  targetComparatorOutcomesList <- list()
+  for (i in seq_along(tcis)) {
+    tci <- tcis[[i]]
+    targetComparatorOutcomesList[[i]] <- createTargetComparatorOutcomes(
+      targetId = tci$targetId,
+      comparatorId = tci$comparatorId,
+      outcomes = outcomeList,
+      excludedCovariateConceptIds = tci$excludedCovariateConceptIds
+    )
+  }
+  getDbCohortMethodDataArgs <- CohortMethod::createGetDbCohortMethodDataArgs(
+    restrictToCommonPeriod = TRUE,
+    studyStartDate = studyStartDate,
+    studyEndDate = studyEndDate,
+    maxCohortSize = 0,
+    covariateSettings = covariateSettings
+  )
+  createPsArgs = CohortMethod::createCreatePsArgs(
+    maxCohortSizeForFitting = 250000,
+    errorOnHighCorrelation = TRUE,
+    stopOnError = FALSE, # Setting to FALSE to allow Strategus complete all CM operations; when we cannot fit a model, the equipoise diagnostic should fail
+    estimator = "att",
+    prior = Cyclops::createPrior(
+      priorType = "laplace",
+      exclude = c(0),
+      useCrossValidation = TRUE
+    ),
+    control = Cyclops::createControl(
+      noiseLevel = "silent",
+      cvType = "auto",
+      seed = 1,
+      resetCoefficients = TRUE,
+      tolerance = 2e-07,
+      cvRepetitions = 1,
+      startingVariance = 0.01
+    )
+  )
+  matchOnPsArgs = CohortMethod::createMatchOnPsArgs(
+    maxRatio = psMatchMaxRatio,
+    caliper = 0.2,
+    caliperScale = "standardized logit",
+    allowReverseMatch = FALSE,
+    stratificationColumns = c()
+  )
+  # stratifyByPsArgs <- CohortMethod::createStratifyByPsArgs(
+  #   numberOfStrata = 5,
+  #   stratificationColumns = c(),
+  #   baseSelection = "all"
+  # )
+  computeSharedCovariateBalanceArgs = CohortMethod::createComputeCovariateBalanceArgs(
+    maxCohortSize = 250000,
+    covariateFilter = NULL
+  )
+  computeCovariateBalanceArgs = CohortMethod::createComputeCovariateBalanceArgs(
+    maxCohortSize = 250000,
+    covariateFilter = FeatureExtraction::getDefaultTable1Specifications()
+  )
+  fitOutcomeModelArgs = CohortMethod::createFitOutcomeModelArgs(
+    modelType = "cox",
+    stratified = psMatchMaxRatio != 1,
+    useCovariates = FALSE,
+    inversePtWeighting = FALSE,
+    prior = Cyclops::createPrior(
+      priorType = "laplace",
+      useCrossValidation = TRUE
+    ),
+    control = Cyclops::createControl(
+      cvType = "auto",
+      seed = 1,
+      resetCoefficients = TRUE,
+      startingVariance = 0.01,
+      tolerance = 2e-07,
+      cvRepetitions = 10,
+      noiseLevel = "quiet"
+    )
+  )
+  cmAnalysisList <- list()
+  for (i in seq_len(nrow(timeAtRisks))) {
+    createStudyPopArgs <- CohortMethod::createCreateStudyPopulationArgs(
+      firstExposureOnly = FALSE,
+      washoutPeriod = 0,
+      removeDuplicateSubjects = "keep first",
+      censorAtNewRiskWindow = TRUE,
+      removeSubjectsWithPriorOutcome = TRUE,
+      priorOutcomeLookback = 99999,
+      riskWindowStart = timeAtRisks$riskWindowStart[[i]],
+      startAnchor = timeAtRisks$startAnchor[[i]],
+      riskWindowEnd = timeAtRisks$riskWindowEnd[[i]],
+      endAnchor = timeAtRisks$endAnchor[[i]],
+      minDaysAtRisk = 1,
+      maxDaysAtRisk = 99999
+    )
+    cmAnalysisList[[i]] <- CohortMethod::createCmAnalysis(
+      analysisId = i,
+      description = sprintf(
+        "Cohort method, %s",
+        timeAtRisks$label[i]
+      ),
+      getDbCohortMethodDataArgs = getDbCohortMethodDataArgs,
+      createStudyPopArgs = createStudyPopArgs,
+      createPsArgs = createPsArgs,
+      matchOnPsArgs = matchOnPsArgs,
+      # stratifyByPsArgs = stratifyByPsArgs,
+      computeSharedCovariateBalanceArgs = computeSharedCovariateBalanceArgs,
+      computeCovariateBalanceArgs = computeCovariateBalanceArgs,
+      fitOutcomeModelArgs = fitOutcomeModelArgs
+    )
+  }
+  cohortMethodModuleSpecifications <- createCohortMethodModuleSpecifications(
+    cmAnalysisList = cmAnalysisList,
+    targetComparatorOutcomesList = targetComparatorOutcomesList,
+    analysesToExclude = NULL,
+    refitPsForEveryOutcome = FALSE,
+    refitPsForEveryStudyPopulation = FALSE,
+    cmDiagnosticThresholds = CohortMethod::createCmDiagnosticThresholds(
+      mdrrThreshold = Inf,
+      easeThreshold = 0.25,
+      sdmThreshold = 0.1,
+      equipoiseThreshold = 0.2,
+      generalizabilitySdmThreshold = 1
+    )
+  )
+
+
+  return(cohortMethodModuleSpecifications)
+}
+
+#' @noRd
+createCmDesign <- function(targetId,
+                           comparatorId,
+                           indicationId,
+                           outcomeCohortIds,
+                           dataSources,
+                           excludedCovariateConceptIds) {
+
+  tcis <- list(
+    #standard analyses that would be performed during routine signal detection
+    list(
+      targetId = targetId, # e.g New users of ACE inhibitors
+      comparatorId = comparatorId, # e.g New users of Alpha-1 Blockers
+      indicationId = indicationId, # e.g Hypertension
+      genderConceptIds = c(8507, 8532), # use valid genders (remove unknown)
+      minAge = NULL, # All ages In years. Can be NULL
+      maxAge = NULL, # All ages In years. Can be NULL
+      excludedCovariateConceptIds = excludedCovariateConceptIds
+    )
+  )
+  outcomes <- tibble::tibble(
+    cohortId = outcomeCohortIds,
+    cleanWindow = c(365)
+  )
+
+  timeAtRisks <- tibble::tibble(
+    label = c("On treatment", "fixed 365d"),
+    riskWindowStart = c(1, 1),
+    startAnchor = c("cohort start", "cohort start"),
+    riskWindowEnd = c(0, 365),
+    endAnchor = c("cohort end", "cohort start"),
+  )
+
+  studyStartDate <- "20010101" # YYYYMMDD, e.g. "2001-02-01" for January 1st, 2001
+  studyEndDate <- "20251231" # YYYYMMDD
+
+  useCleanWindowForPriorOutcomeLookback <- FALSE # If FALSE, lookback window is all time prior, i.e., including only first events
+  psMatchMaxRatio <- 1 # If bigger than 1, the outcome model will be conditioned on the matched set
+
+  covariateSettings <- FeatureExtraction::createDefaultCovariateSettings(
+    addDescendantsToExclude = TRUE # Keep TRUE because you're excluding concepts
+  )
+  outcomeList <-
+    lapply(seq_len(nrow(outcomes)), function(i) {
+      if (useCleanWindowForPriorOutcomeLookback)
+        priorOutcomeLookback <- outcomes$cleanWindow[i]
+      else
+        priorOutcomeLookback <- 99999
+      CohortMethod::createOutcome(
+        outcomeId = outcomes$cohortId[i],
+        outcomeOfInterest = TRUE,
+        trueEffectSize = NA,
+        priorOutcomeLookback = priorOutcomeLookback
+      )
+    })
+
+  targetComparatorOutcomesList <- list()
+  for (i in seq_along(tcis)) {
+    tci <- tcis[[i]]
+    targetComparatorOutcomesList[[i]] <- CohortMethod::createTargetComparatorOutcomes(
+      targetId = tci$targetId,
+      comparatorId = tci$comparatorId,
+      outcomes = outcomeList,
+      excludedCovariateConceptIds = tci$excludedCovariateConceptIds
+    )
+  }
+  getDbCohortMethodDataArgs <- CohortMethod::createGetDbCohortMethodDataArgs(
+    restrictToCommonPeriod = TRUE,
+    studyStartDate = studyStartDate,
+    studyEndDate = studyEndDate,
+    maxCohortSize = 0,
+    covariateSettings = covariateSettings
+  )
+  createPsArgs = CohortMethod::createCreatePsArgs(
+    maxCohortSizeForFitting = 250000,
+    errorOnHighCorrelation = TRUE,
+    stopOnError = FALSE, # Setting to FALSE to allow Strategus complete all CM operations; when we cannot fit a model, the equipoise diagnostic should fail
+    estimator = "att",
+    prior = Cyclops::createPrior(
+      priorType = "laplace",
+      exclude = c(0),
+      useCrossValidation = TRUE
+    ),
+    control = Cyclops::createControl(
+      noiseLevel = "silent",
+      cvType = "auto",
+      seed = 1,
+      resetCoefficients = TRUE,
+      tolerance = 2e-07,
+      cvRepetitions = 1,
+      startingVariance = 0.01
+    )
+  )
+  matchOnPsArgs = CohortMethod::createMatchOnPsArgs(
+    maxRatio = psMatchMaxRatio,
+    caliper = 0.2,
+    caliperScale = "standardized logit",
+    allowReverseMatch = FALSE,
+    stratificationColumns = c()
+  )
+  # stratifyByPsArgs <- CohortMethod::createStratifyByPsArgs(
+  #   numberOfStrata = 5,
+  #   stratificationColumns = c(),
+  #   baseSelection = "all"
+  # )
+  computeSharedCovariateBalanceArgs = CohortMethod::createComputeCovariateBalanceArgs(
+    maxCohortSize = 250000,
+    covariateFilter = NULL
+  )
+  computeCovariateBalanceArgs = CohortMethod::createComputeCovariateBalanceArgs(
+    maxCohortSize = 250000,
+    covariateFilter = FeatureExtraction::getDefaultTable1Specifications()
+  )
+  fitOutcomeModelArgs = CohortMethod::createFitOutcomeModelArgs(
+    modelType = "cox",
+    stratified = psMatchMaxRatio != 1,
+    useCovariates = FALSE,
+    inversePtWeighting = FALSE,
+    prior = Cyclops::createPrior(
+      priorType = "laplace",
+      useCrossValidation = TRUE
+    ),
+    control = Cyclops::createControl(
+      cvType = "auto",
+      seed = 1,
+      resetCoefficients = TRUE,
+      startingVariance = 0.01,
+      tolerance = 2e-07,
+      cvRepetitions = 10,
+      noiseLevel = "quiet"
+    )
+  )
+  cmAnalysisList <- list()
+  for (i in seq_len(nrow(timeAtRisks))) {
+    createStudyPopArgs <- CohortMethod::createCreateStudyPopulationArgs(
+      firstExposureOnly = FALSE,
+      washoutPeriod = 0,
+      removeDuplicateSubjects = "keep first",
+      censorAtNewRiskWindow = TRUE,
+      removeSubjectsWithPriorOutcome = TRUE,
+      priorOutcomeLookback = 99999,
+      riskWindowStart = timeAtRisks$riskWindowStart[[i]],
+      startAnchor = timeAtRisks$startAnchor[[i]],
+      riskWindowEnd = timeAtRisks$riskWindowEnd[[i]],
+      endAnchor = timeAtRisks$endAnchor[[i]],
+      minDaysAtRisk = 1,
+      maxDaysAtRisk = 99999
+    )
+    cmAnalysisList[[i]] <- CohortMethod::createCmAnalysis(
+      analysisId = i,
+      description = sprintf(
+        "Cohort method, %s",
+        timeAtRisks$label[i]
+      ),
+      getDbCohortMethodDataArgs = getDbCohortMethodDataArgs,
+      createStudyPopArgs = createStudyPopArgs,
+      createPsArgs = createPsArgs,
+      matchOnPsArgs = matchOnPsArgs,
+      # stratifyByPsArgs = stratifyByPsArgs,
+      computeSharedCovariateBalanceArgs = computeSharedCovariateBalanceArgs,
+      computeCovariateBalanceArgs = computeCovariateBalanceArgs,
+      fitOutcomeModelArgs = fitOutcomeModelArgs
+    )
+  }
+  cohortMethodModuleSpecifications <- createCohortMethodModuleSpecifications(
+    cmAnalysisList = cmAnalysisList,
+    targetComparatorOutcomesList = targetComparatorOutcomesList,
+    analysesToExclude = NULL,
+    refitPsForEveryOutcome = FALSE,
+    refitPsForEveryStudyPopulation = FALSE,
+    cmDiagnosticThresholds = CohortMethod::createCmDiagnosticThresholds(
+      mdrrThreshold = Inf,
+      easeThreshold = 0.25,
+      sdmThreshold = 0.1,
+      equipoiseThreshold = 0.2,
+      generalizabilitySdmThreshold = 1
+    )
+    #,dataSources = dataSources
+  )
+
+
+  return(cohortMethodModuleSpecifications)
+}
+
+
 
 #' Execute Cohort method analysis
 #' @description
@@ -13,27 +395,26 @@ executeCohortMethodAnalysis <- function(config, cmConfig) {
   }
   connection <- DatabaseConnector::connect(config$connectionDetails)
   on.exit(DatabaseConnector::disconnect(connection))
-  # Get outcome cohorts
-  cmConfig$settings <- createCmDesign(targetId = cmConfig$targetId,
-                                      comparatorId = cmConfig$comparatorId,
-                                      indicationId = cmConfig$indicationId,
-                                      outcomeCohortIds =  getAtlasOutcomeIds(connection, config),
-                                      excludedCovariateConceptIds = cmConfig$excludedCovariateConceptIds)
 
-  cmConfig$settings$connectionDetails <- config$connectionDetails
-  cmConfig$settings$cdmDatabaseSchema <- config$cdmSchema
-  cmConfig$settings$exposureDatabaseSchema <- config$resultSchema
-  cmConfig$settings$exposureTable <- config$tables$cohort
-  cmConfig$settings$outcomeDatabaseSchema <- config$resultSchema
-  cmConfig$settings$outcomeTable <- config$tables$cohort
-  cmConfig$settings$outputFolder <- file.path(config$workDir, "CohortMethodOutput")
-  cmConfig$settings$multiThreadingSettings <- multiThreadingSettings
-  cmConfig$settings$cmDiagnosticThresholds  <- NULL
+  settings <- createCmDesign(targetId = cmConfig$targetId,
+                             comparatorId = cmConfig$comparatorId,
+                             indicationId = cmConfig$indicationId,
+                             outcomeCohortIds = getAtlasOutcomeIds(connection, config),
+                             excludedCovariateConceptIds = cmConfig$excludedCovariateConceptIds)$settings
 
-  do.call(CohortMethod::runCmAnalyses, cmConfig$settings)
+  settings$connectionDetails <- config$connectionDetails
+  settings$cdmDatabaseSchema <- config$cdmSchema
+  settings$exposureDatabaseSchema <- config$resultSchema
+  settings$exposureTable <- config$tables$cohort
+  settings$outcomeDatabaseSchema <- config$resultSchema
+  settings$outcomeTable <- config$tables$cohort
+  settings$outputFolder <- file.path(config$workDir, "CohortMethodOutput")
+  settings$multiThreadingSettings <- multiThreadingSettings
+  settings$cmDiagnosticThresholds  <- NULL
+  do.call(CohortMethod::runCmAnalyses, settings)
 
   tryCatch({
-    CohortMethod::exportToCsv(outputFolder = cmConfig$settings$outputFolder,
+    CohortMethod::exportToCsv(outputFolder = settings$outputFolder,
                               exportFolder = config$exportPath,
                               databaseId = config$sourceId,
                               maxCores = parallel::detectCores() - 1)
